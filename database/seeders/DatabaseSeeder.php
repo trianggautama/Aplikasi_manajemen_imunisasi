8f0bdef4a8a5a94261b1d5046407fb968d2df3e7
muhammadasadbastomi <tomycules@gmail.com>
2021-06-05 15:17:35 +0800

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('user')->insert([
+            'username' => 'admin',
+            'password' => Hash::make('123'),
+            'role' => '1',
+        ]);
+
+        DB::table('pegawai')->insert([
+            'user_id' => '1',
+            'nama' => 'Nama Admin',
+            'alamat' => 'Pelaihari',
+            'jk' => 'Laki-laki',
+            'tempat_lahir' => 'Pelaihari',
+            'no_hp' => '087771787133',
+            'tgl_lahir' => Carbon::parse('2000-01-01'),
+        ]);
+
     }
 }
