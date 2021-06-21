0571d5b683861006cc10ef13198665584b862f53
muhammadasadbastomi <tomycules@gmail.com>
2021-06-21 17:29:11 +0800

    update

--- resources/views/laporan/imunisasi_show.blade.php
+++ resources/views/laporan/imunisasi_show.blade.php
@@ -1,111 +1,133 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-    *{
-        font-family:arial;
-    }
-    body{
-        font-family:arial !important;
+        * {
+            font-family: arial;
         }
-    table{
-        border-collapse: collapse;
-        width:100%;
-      }
-    table, th, td{
-        border: 1px solid #708090;
-      }
-      th{
-        /* text-align: center; */
-        height:40px;
-      }
-      td{
-        text-align: center;
-        padding:2px;
-      }
-      br{
-          margin-bottom: 5px !important;
-      }
-     .judul{
-         text-align: center;
-     }
-     .header{
-         text-align: center;
-         padding: 0px;
-     }
-     .pemko{
-         width:80px;
-     }
-     .bakti_husada{
-         width:140px;
-     }
-     .logo{
-         float: left;
-         margin-right: 0px;
-         width: 15%;
-         padding:0px;
-         text-align: right;
-     }
-     .headtext{
-         /* float:right; */
-         margin-left: 0px;
-         width: 100%;
-         padding-left:60px !important;
-         padding-right:8%;
-     }
-     hr{
-         height: 3px;
-         background-color: black;
-     }
-     .ttd{
-         margin-left:70%;
-         text-align: center;
-         text-transform: uppercase;
-     }
-     .text-center{
-       text-align:center;
-     }
 
-     @page { 
-        size: 21.5 cm 29 cm ; 
-         margin: 1 cm 1 cm 0 cm 1 cm !important;
-         padding: 0px !important;
-         }
+        body {
+            font-family: arial !important;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid #708090;
+        }
+
+        th {
+            /* text-align: center; */
+            height: 40px;
+        }
+
+        td {
+            text-align: center;
+            padding: 2px;
+        }
+
+        br {
+            margin-bottom: 5px !important;
+        }
+
+        .judul {
+            text-align: center;
+        }
+
+        .header {
+            text-align: center;
+            padding: 0px;
+        }
+
+        .pemko {
+            width: 80px;
+        }
+
+        .bakti_husada {
+            width: 140px;
+        }
+
+        .logo {
+            float: left;
+            margin-right: 0px;
+            width: 15%;
+            padding: 0px;
+            text-align: right;
+        }
+
+        .headtext {
+            /* float:right; */
+            margin-left: 0px;
+            width: 100%;
+            padding-left: 60px !important;
+            padding-right: 8%;
+        }
+
+        hr {
+            height: 3px;
+            background-color: black;
+        }
+
+        .ttd {
+            margin-left: 70%;
+            text-align: center;
+            text-transform: uppercase;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        @page {
+            size: 21.5 cm 29 cm;
+            margin: 1 cm 1 cm 0 cm 1 cm !important;
+            padding: 0px !important;
+        }
     </style>
 </head>
+
 <body>
-<div class="header" style="padding=0px; margin:0px">
-            <table style="border: none"> 
-                <tr style="border: none">
-                    <td style="width:20%; border: none;"> <img  class="pemko" src="asset_template/images/pemkab.png"></td>
-                    <td style="width:60%; border: none">
-                        <h4 style="margin:0px; border: none">PEMERINTAH KABUPATEN TANAH LAUT </h4>
-                        <h2 style="margin:0px; border: none">DINAS KESEHATAN </h2>
-                        <h2 style="margin:0px; border: none">PUSKESMAS PELAIHARI </h2>
-                        <p style="margin:0px; border: none">Karang Taruna, Pelaihari, Tanah Laut, Pelaihari, Tanah Laut, Kabupatèn Tanah Laut, Kalimantan Kidul 70812</p>
-                    </td>
-                    <td style="width:20%; border: none"><img  class="bakti_husada" src="asset_template/images/logo.png" width="100%"></td>
-                </tr>
-            </table>
-            <hr>
+    <div class="header" style="padding=0px; margin:0px">
+        <table style="border: none">
+            <tr style="border: none">
+                <td style="width:20%; border: none;"> <img class="pemko" src="asset_template/images/pemkab.png"></td>
+                <td style="width:60%; border: none">
+                    <h4 style="margin:0px; border: none">PEMERINTAH KABUPATEN TANAH LAUT </h4>
+                    <h2 style="margin:0px; border: none">DINAS KESEHATAN </h2>
+                    <h2 style="margin:0px; border: none">PUSKESMAS PELAIHARI </h2>
+                    <p style="margin:0px; border: none">Karang Taruna, Pelaihari, Tanah Laut, Pelaihari, Tanah Laut,
+                        Kabupatèn Tanah Laut, Kalimantan Kidul 70812</p>
+                </td>
+                <td style="width:20%; border: none"><img class="bakti_husada" src="asset_template/images/logo.png"
+                        width="100%"></td>
+            </tr>
+        </table>
+        <hr>
     </div>
     <div class="container" style="padding=0px; margin:0px">
         <div class="isi" style="padding=0px; margin:0px">
-        <h3 class="headtext" style="text-align: center;">Data Detail Imunisasi</h3>
+            <h3 class="headtext" style="text-align: center;">Data Detail Imunisasi</h3>
             <table style="border: none">
                 <tr style="border: none">
                     <td style=" text-align: left; width: 24%;border: none;">Jenis Vaksin</td>
                     <td style="text-align: left; border: none">: {{$data->vaksin->nama_vaksin}}</td>
                 </tr>
-                <tr style="border: none"> 
+                <tr style="border: none">
                     <td style="text-align: left;border: none">Tanggal Imunisasi</td>
-                    <td style="text-align: left; border: none">: {{Carbon\carbon::parse($data->tanggal_informasi)->translatedFormat('d F Y')}}</td>
+                    <td style="text-align: left; border: none">:
+                        {{Carbon\carbon::parse($data->tanggal_informasi)->translatedFormat('d F Y')}}</td>
                 </tr>
-                <tr style="border: none"> 
+                <tr style="border: none">
                     <td style="text-align: left;border: none">Tempat Imunisasi</td>
                     <td style="text-align: left; border: none">: {{$data->tempat_imunisasi}}</td>
                 </tr>
@@ -137,9 +159,9 @@
                         </td>
                         <td>{{$k->tinggi_badan}} cm</td>
                         <td>{{$k->berat_badan}} kg</td>
-                        <td>{{$k->umur}} bulan</td>
+                        <td>{{$k->umur}}</td>
                     </tr>
-                    @endforeach  
+                    @endforeach
                 </tbody>
             </table>
             <br>
@@ -147,22 +169,23 @@
             <table style="border: none">
                 <tr style="border: none">
                     <td style="width: 60%;border: none;">
-                        
+
                     </td>
                     <td style="border: none">
-                    Pelaihari , {{Carbon\carbon::now()->translatedFormat('d F Y')}}
-                    <br><br>
-                    <p style="margin:0px;">Kepala Puskesmas</p>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <p style="margin:0px;">.......................</p>
+                        Pelaihari , {{Carbon\carbon::now()->translatedFormat('d F Y')}}
+                        <br><br>
+                        <p style="margin:0px;">Kepala Puskesmas</p>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <p style="margin:0px;">.......................</p>
                     </td>
                 </tr>
             </table>
 
         </div>
-    </div>      
+    </div>
 </body>
+
 </html>
